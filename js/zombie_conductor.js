33e28b8823e2907eec66187293cbc81d2a1cc382
lucasdellasala <lucasdellasalamarkovic@gmail.com>
2020-02-10 20:01:19 -0300

    Creando clases de zombie h y v

--- js/zombie_conductor.js
+++ js/zombie_conductor.js
@@ -6,7 +6,8 @@ del ZombieCaminante que eran los mismos. */
 
 var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov, direccion) {
   /* Completar constructor a partir de Enemigo */
-  Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov, direccion);  
+  Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov);  
+  this.direccion = direccion;
   
   /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
 }
@@ -15,3 +16,27 @@ var ZombieConductor = function(sprite, x, y, ancho, alto, velocidad, rangoMov, d
 ZombieConductor.prototype = Object.create(Enemigo.prototype);
 ZombieConductor.prototype.constructor = ZombieConductor;
 /* Completar metodos para el movimiento y el ataque */
+
+ZombieConductor.prototype.mover = function() {
+    
+  if(this.direccion==="h"){
+    this.x += this.velocidad;
+    if ((this.x < this.rangoMov.desdeX) || (this.x > this.rangoMov.hastaX)){
+      this.velocidad *= -1;
+    }
+  }else{
+    this.y += this.velocidad;
+    if ((this.y < this.rangoMov.desdeY) || (this.y > this.rangoMov.hastaY)){
+      this.velocidad *= -1;
+    }
+  }
+  
+  
+  
+  
+}
+
+
+
+  
+  
